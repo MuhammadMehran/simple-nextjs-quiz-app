4ddf1dff4430c19e3044c0e5936cd36ea53ec413
Muhammad Mehran <42321153+MuhammadMehran@users.noreply.github.com>
2024-05-02 14:36:34 +0500

    add options onclick

--- components/options.js
+++ components/options.js
@@ -1,14 +1,29 @@
 import React from "react";
 
-const Options = ({ answerOptions }) => {
+const Options = ({
+  answerOptions,
+  handleAnswerOption,
+  selectedOptions,
+  currentQuestion,
+}) => {
   return (
     <div className="flex flex-col w-full">
       {answerOptions.map((answer, index) => (
         <div
           key={index}
+          onClick={(e) => handleAnswerOption(answer.answer)}
           className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-white/5 border-white/10 rounded-xl"
         >
-          <input type="radio" className="w-6 h-6 bg-black" />
+          <input
+            type="radio"
+            name={answer.answer}
+            value={answer.answer}
+            onChange={(e) => handleAnswerOption(answer.answer)}
+            checked={
+              answer.answer === selectedOptions[currentQuestion]?.answerByUser
+            }
+            className="w-6 h-6 bg-black"
+          />
           <p className="ml-6 text-white">{answer.answer}</p>
         </div>
       ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
   const handlePrevious = () => {
     const prevQues = currentQuestion - 1;
@@ -20,20 +23,30 @@ export default function Home() {
     const nextQues = currentQuestion + 1;
     nextQues < QUESTIONS.length && setCurrentQuestion(nextQues);
   };
+
+  const handleAnswerOption = (answer) => {
+    setSelectedOptions([
+      (selectedOptions[currentQuestion] = { answerByUser: answer }),
+    ]);
+    setSelectedOptions([...selectedOptions]);
+  };
   return (
     <div className="flex flex-col  w-screen px-5 h-screen bg-[#1A1A1A] justify-center items-center">
       <Head>
         <title>Quiz App</title>
       </Head>
-      {/* {QUESTIONS.map((value, index) => (
-        <Question />
-      ))} */}
+
       <Question
         q={QUESTIONS[currentQuestion].question}
         currentQuestion={currentQuestion + 1}
         totalQuestions={QUESTIONS.length}
       />
-      <Options answerOptions={QUESTIONS[currentQuestion].answerOptions} />
+      <Options
+        answerOptions={QUESTIONS[currentQuestion].answerOptions}
+        handleAnswerOption={handleAnswerOption}
+        selectedOptions={selectedOptions}
+        currentQuestion={currentQuestion}
+      />
 
       <div className="flex justify-between w-full mt-4 text-white">
         <button
