8b14ff6c147889f7199641d19e060afba6eb86cd
Muhammad Mehran <42321153+MuhammadMehran@users.noreply.github.com>
2024-05-02 14:24:45 +0500

    implement onclick for next & previous buttons

--- components/question.js
+++ components/question.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Question = ({ q }) => {
+const Question = ({ q, currentQuestion, totalQuestions }) => {
   return (
     <div className="flex flex-col items-start w-full">
-      <h4 className="mt-10 text-xl text-white/60">Question 1 of 5</h4>
+      <h4 className="mt-10 text-xl text-white/60">
+        Question {currentQuestion} of {totalQuestions}
+      </h4>
       <div className="mt-4 text-2xl text-white">{q}</div>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,22 @@ import Head from "next/head";
 import Question from "../../components/question";
 import Options from "../../components/options";
 import { QUESTIONS } from "../../lib/questions";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handlePrevious = () => {
+    const prevQues = currentQuestion - 1;
+    prevQues >= 0 && setCurrentQuestion(prevQues);
+  };
+
+  const handleNext = () => {
+    const nextQues = currentQuestion + 1;
+    nextQues < QUESTIONS.length && setCurrentQuestion(nextQues);
+  };
   return (
     <div className="flex flex-col  w-screen px-5 h-screen bg-[#1A1A1A] justify-center items-center">
       <Head>
@@ -16,14 +28,26 @@ export default function Home() {
       {/* {QUESTIONS.map((value, index) => (
         <Question />
       ))} */}
-      <Question q={QUESTIONS[0].question} />
-      <Options answerOptions={QUESTIONS[0].answerOptions} />
+      <Question
+        q={QUESTIONS[currentQuestion].question}
+        currentQuestion={currentQuestion + 1}
+        totalQuestions={QUESTIONS.length}
+      />
+      <Options answerOptions={QUESTIONS[currentQuestion].answerOptions} />
 
       <div className="flex justify-between w-full mt-4 text-white">
-        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg">
+        <button
+          className="w-[49%] py-3 bg-indigo-600 rounded-lg"
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button className="w-[49%] py-3 bg-indigo-600 rounded-lg">Next</button>
+        <button
+          className="w-[49%] py-3 bg-indigo-600 rounded-lg"
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
